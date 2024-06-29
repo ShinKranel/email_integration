9b2c6ddb3692f575e7d4dbe901caeaf1385daeb8
Shin Kranel <shinkranel@gmail.com>
2024-06-29 19:35:03 +0100

    Add get_emails_router to main.py, moved models to get_emails dir

--- backend/src/get_emails/models.py
+++ backend/src/get_emails/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -10,7 +10,9 @@ from backend.src.db import Base
 class User(Base):
     __tablename__ = 'user'
 
-    login: Mapped[str] = mapped_column(unique=True)
+    login: Mapped[str] = mapped_column(
+        primary_key=True, unique=True, nullable=False
+    )
     password: Mapped[str]
 
     emails = relationship('Email', back_populates='user')
@@ -19,12 +21,14 @@ class User(Base):
 class Email(Base):
     __tablename__ = 'email'
 
-    id: Mapped[str]
+    id: Mapped[str] = mapped_column(primary_key=True)
     email_theme: Mapped[str]
     send_date: Mapped[datetime]
     receive_date: Mapped[datetime]
     email_text: Mapped[str]
     attached_files: Mapped[ARRAY] = mapped_column(ARRAY(String))
 
-    user_id: Mapped[str] = mapped_column(foreign_key='user.id')
+    user_id: Mapped[str] = mapped_column(
+        ForeignKey('user.login', ondelete='CASCADE'), nullable=False
+    )
     user = relationship('User', back_populates='emails')
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 from backend.src.config import settings
+from backend.src.get_emails.router import router as get_emails_router
 
 app = FastAPI(
     title=settings.PROJECT_TITLE
 )
+
+app.include_router(get_emails_router, prefix="/get_emails", tags=["get_emails"])
